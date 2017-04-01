0ca5a50da8ab1f8711afd8977960c1c307e046db
nickscript0 <nickscript0@users.noreply.github.com>
2017-04-01 13:06:55 -0600

    working multi-colour highlight

--- src/highlight.ts
+++ src/highlight.ts
@@ -23,9 +23,11 @@ export interface Highlight {
 
 export class TextHighlight implements Highlight {
     private words: Set<string>;
+    private colour: ColourState;
 
     constructor() {
         this.words = new Set();
+        this.colour = new ColourState();
     }
 
     add() {
@@ -42,34 +44,48 @@ export class TextHighlight implements Highlight {
     private _highlightWord(word: string) {
         const comments = getAllComments();
         let highlight_count = 0;
-        Array.from(comments).forEach(e => {
-            const text_els = e.getElementsByClassName('c00');
-            if (text_els.length === 0) return;
+        const highlight_colour = this.colour.next();
+        Array.from(comments).forEach(all_el => {
+            const text_els = all_el.getElementsByClassName('c00');
+            Array.from(text_els).forEach(original_node => {
+                if (original_node.textContent === null) return;
+                const word_index = original_node.textContent.indexOf(word);
+                if (word_index === -1) return;
 
-            const original_node = text_els[0];
+                // Build 3 new nodes to replace the original_node:
+                // pre_node, highlight_node, post_node
+                const pre_node = original_node.cloneNode();
+                pre_node.textContent = original_node.textContent.substring(0, word_index);
+                const post_node = original_node.cloneNode();
+                post_node.textContent = original_node.textContent.substring(word_index + word.length);
 
-            if (original_node.textContent === null) return;
-            const word_index = original_node.textContent.indexOf(word);
-            if (word_index === -1) return;
+                original_node.textContent = word;
+                const html_element = <HTMLElement>original_node;
+                html_element.style.color = highlight_colour;
+                html_element.style.fontWeight = 'bold';
 
-            // Build 3 new nodes to replace the original_node:
-            // pre_node, highlight_node, post_node
-            const pre_node = original_node.cloneNode();
-            pre_node.textContent = original_node.textContent.substring(0, word_index);
-            const post_node = original_node.cloneNode();
-            post_node.textContent = original_node.textContent.substring(word_index + word.length);
-
-            original_node.textContent = word;
-            const html_element = <HTMLElement>original_node;
-            html_element.style.color = 'DarkOrchid';
-            html_element.style.fontWeight = 'bold';
-
-            // Replace original_node with the new nodes
-            if (original_node.parentNode === null) return;
-            original_node.parentNode.insertBefore(pre_node, original_node);
-            original_node.parentNode.insertBefore(post_node, original_node.nextSibling);
-            highlight_count++;
+                // Replace original_node with the new nodes
+                if (original_node.parentNode === null) return;
+                original_node.parentNode.insertBefore(pre_node, original_node);
+                original_node.parentNode.insertBefore(post_node, original_node.nextSibling);
+                highlight_count++;
+            });
         });
         console.log(`Highlighted ${word} ${highlight_count} times`);
+    }
+}
+
+const HIGHLIGHT_COLOURS = ['DarkOrchid', 'DarkCyan', 'Crimson', 'DarkGoldenRod', 'DarkOrange', 'Fuchsia'];
+
+class ColourState {
+    colour_index: number;
+
+    constructor() {
+        this.colour_index = 0;
+    }
+
+    next() {
+        if (this.colour_index >= HIGHLIGHT_COLOURS.length) this.colour_index = HIGHLIGHT_COLOURS.length - 1;
+        return HIGHLIGHT_COLOURS[this.colour_index++];
     }
 }
