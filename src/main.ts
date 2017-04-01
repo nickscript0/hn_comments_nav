ae814722abe794501c1baaa97c7e2abaeb4d8740
nickscript0 <nickscript0@users.noreply.github.com>
2017-04-01 11:40:39 -0600

    extract highlight_op to module

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,14 @@
  * HN Keynav Chrome Extension
  */
 
-import {BrowserNav, Nav} from "browser_nav";
+import { BrowserNav, Nav } from "browser_nav";
+import { highlight_op } from "highlight";
 
 function main() {
     const nav = new BrowserNav();
     document.onkeypress = handle_key(nav);
     highlight_op();
 }
-
-/**
- * Highlight the original poster's name throughout the page
- */
-function highlight_op() {
-    const op_name = document.getElementsByClassName('subtext')[0]
-        .getElementsByClassName('hnuser')[0]
-        .textContent;
-    Array.from(document.getElementsByClassName('hnuser'))
-        .filter(e => e.textContent === op_name)
-        .map(element => {
-            const html_element = <HTMLElement>element;
-            html_element.style.color = '#42c135';
-            html_element.style.fontWeight = 'bold';
-        });
-}
-
-
 
 function handle_key(nav: Nav) {
     const key_map = {
